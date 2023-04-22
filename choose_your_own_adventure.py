5f6b001006a9fb01d963e5022c74b04f5bb4fa1d
Benjamin Schroeder <benjamin.schroeder587@gmail.com>
2023-04-22 19:26:21 +0200

    changed or to and condition

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,8 +1,8 @@
 print('(Insert Story which ends in choice of left through narrow alley or right through gardens)')
 choice = input()
-if choice == 'left':
+if choice != 'left' and choice != 'right':
+    print('please choose left or right')
+elif choice == 'left':
     print('(Insert story through narrow alley)')
 elif choice == 'right':
     print('(Insert story through gardens)')
-elif choice != ('left' or 'right'):
-    print('please choose left or right')
