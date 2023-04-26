5eb5a8a2960e3b91142aacab568cb85ff7b291d8
Benjamin Schroeder <benjamin.schroeder587@gmail.com>
2023-04-26 23:40:39 +0200

    fixed the infinite loop problem

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -1,8 +1,11 @@
 print('(Insert Story which ends in choice of left through narrow alley or right through gardens)')
 choice = input()
-if choice != 'left' and choice != 'right':
+while choice != 'left' and choice != 'right':
     print('please choose left or right')
-elif choice == 'left':
+    choice = input()
+    if choice == 'left' and choice == 'right':
+        break
+if choice == 'left':
     print('(Insert story through narrow alley)')
 elif choice == 'right':
     print('(Insert story through gardens)')
